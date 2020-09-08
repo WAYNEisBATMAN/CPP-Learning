a7bd48cf8d0f35c4a1a6a539c9f64b5db2c41b96
WAYNEisBATMAN <64163400+WAYNEisBATMAN@users.noreply.github.com>
2020-09-09 00:05:57 +0530

    vsc

--- 1 Tokens/2. Operators.cpp
+++ 1 Tokens/2. Operators.cpp
@@ -4,20 +4,19 @@ An operator is a symbol that tells the compiler to perform specific mathematical
 Operators are symbols that triggers an action when applied to C variables and other objects. 
 The data items on which operators act upon are called operands.
 Depending on the number of operands that an operator can act upon, operators can be classified as follows:
-
+//------------------------------------------------------------------------------------------------------------------------
 
 . i) Unary Operators: 
 Those operators that require only single operand to act upon are known as unary operators.For Example increment and 
 decrement operators
 //------------------------------------------------------------------------------------------------------------------------
 
-
 .ii) Binary Operators: 
 Those operators that require two operands to act upon are called binary operators. 
 
 Binary operators are classified into :
 
-*(a) Arithmetic operators
+*(a) Arithmetic Operators
 (+, -, *, /, %, post-increment, pre-increment, post-decrement, pre-decrement)
 
 *(b) Relational Operators
@@ -29,11 +28,11 @@ Binary operators are classified into :
 *(d) Assignment Operators
 It is used to assign values. 
 (=, +=, -=, *=, /=, %=, <<, >>=, &=, ^=, |= )	 
-! For explanation of each operator, go to the links down below. 
 
 *(e) Bitwise Operators       
 <<, !=, >>, ~, |, &, ^ 
 
+! For explanation of each operator, go to the links down below. 
 //------------------------------------------------------------------------------------------------------------------------
 
 .iii) Ternary operators:
